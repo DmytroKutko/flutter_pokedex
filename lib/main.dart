93de2761c9924d86fbac6a87b650513124397cf8
dmytrokutko <dmytrokutko@gmail.com>
2024-07-11 13:48:31 +0300

    fix main fun

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:pokedex/di/dependency_injection.dart';
 import 'package:pokedex/feature/pokemons/presentation/pages/pokedex_page.dart';
 
-void main() async{
+void main() {
   setupLocator();
   runApp(const MyApp());
 }
